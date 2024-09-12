bf796dcddd1743701a74549a064dfd1db8b66bf1
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-12 18:03:00 +0100

    commit

---
+++ Série01/exercice10.py
@@ -0,0 +1,11 @@
+
+nb1 = float(input("nb1 : "))
+S = nb1
+nb1 = float(input("nb2 : "))
+S = nb1
+nb1 = float(input("nb3 : "))
+S = nb1
+nb1 = float(input("nb4 : "))
+S = nb1
+print(f"la somme est {S}")
+
