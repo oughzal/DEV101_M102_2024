caf8956821b20e98c7a65c5fb4ef3fcb74b44dfb
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-12 17:44:24 +0100

    commit

---
+++ Série01/Exercice9.py
@@ -0,0 +1,14 @@
+# //Entrée
+# Ecrire("Xa : ")
+# Lire(Xa)
+# Ecrire("Ya : ")
+# Lire(Ya)
+# Ecrire("Xb : ")
+# Lire(Xb)
+# Ecrire("Yb : ")
+# Lire(Yb)
+# //Traitement
+# D = racine((Xa-Xb)^2 + (Ya-Yb)^2)
+# D = ((Xa-Xb)^2 + (Ya-Yb)^2)^0.5
+# //Sortie
+#     Ecrire("La distance est ", D)
