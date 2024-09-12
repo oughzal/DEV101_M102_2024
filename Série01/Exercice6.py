a1ea6835e5b49415be0f63dc7392f548119cfea7
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-12 15:25:45 +0100

    commit

---
+++ Série01/Exercice6.py
@@ -0,0 +1,13 @@
+# //Entrée
+a=float(input("donner la valeur de a : "))
+b=float(input ("donner la valeur de b;"))
+# //Traitement
+s = a+b
+p = a*b
+st = a-b
+d = a/b # b # 0
+# //Sortie
+print ("a somme est ",s)
+print("Le produit est ", p)
+print("La soustraction est ", st)
+print("La division est ", d)
