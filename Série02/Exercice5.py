0d301e45418ee2376083270c3bfe45e3d3e06ea6
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-18 14:14:12 +0100

    Commit

---
+++ Série02/Exercice5.py
@@ -0,0 +1,8 @@
+a=float(input("donner le nomber a : "))
+b=float(input("donner le nomber b : "))
+if a!=0:
+    print("x=",-b/a)
+elif b==0:
+    print("la solution est l'ensemble R")
+else :
+    print("la solution est l'ensemble vide")
