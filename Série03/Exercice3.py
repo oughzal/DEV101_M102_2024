9370c933e0f2b13de088fc84c6cb83deced54c05
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-20 18:48:20 +0100

    commit

---
+++ Série03/Exercice3.py
@@ -0,0 +1,5 @@
+n=int(input("donner la valeur de n"))
+Un = 1 # Un = U0
+for i in range(1,n+1) :
+    Un = 4*Un +6
+print ("U",n, " = ", Un)
---
+++ Série03/exercice4.py
@@ -0,0 +1,5 @@
+n = int(input("Donner n : "))
+s = 0
+for i in range(1,n+1):
+    s = s + i**2
+print(f"La somme est {s}")
