0f288bcae06034ad03d458756df2ea9d3945f8a5
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-23 15:36:19 +0100

    commit

--- Série03/Exercice5.py
+++ Série03/Exercice5.py
@@ -1,4 +1,6 @@
 N = int(input("Entrer N : "))
+
+#Méthode 1
 S = 0
 if N % 2 == 0 :
     for i in range(0,N+1,2):
@@ -7,6 +9,7 @@ else :
     for i in range(1,N+1,2):
         S = S + i 
 print("la somme S = ",S) 
+#Méthode 2
 S = 0
 for i in range(1,N+1):
     if N % 2 == 0 and i % 2 == 0:
@@ -15,8 +18,8 @@ for i in range(1,N+1):
         S = S+i
 
 print("La somme S = ",S)
+#Méthode 3
 S= 0
 for i in range (N,0,-2):
     S= S+i
 print("la somme est :", S)
-print(f"La somme est {sum([i for i in range(N,0,-2)])}")
