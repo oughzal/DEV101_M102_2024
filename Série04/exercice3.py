9cea79f5c638d652df0c10b41db46cf5d47edaee
omar OUGHZAL <omar.oughzal@gmail.com>
2024-09-27 17:46:09 +0100

    commit

---
+++ Série04/exercice3.py
@@ -0,0 +1,22 @@
+N = int(input("donner le nombre des elements: "))
+
+T = []
+
+for i in range(N):
+    V=int(input(f"Donner l element {i}:"))
+    T.append(V)
+S,P=0,1
+
+for i in range(len(T)):
+    S+=T[i]
+    P*=T[i]
+S,P=0,1
+#T =[5,9,4]
+for e in T:
+    S+= e
+    P*=e
+M = S / len(T)
+
+print("La somme = ",S)
+print("La Produit = ",P)
+print("La moyenne = ",M)
