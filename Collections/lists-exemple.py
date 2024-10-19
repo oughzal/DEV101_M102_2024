ee863022f35dc01984c80c10ee7aac25885130e1
omar OUGHZAL <omar.oughzal@gmail.com>
2024-10-19 03:04:57 +0100

    commit

---
+++ Collections/lists-exemple.py
@@ -0,0 +1,80 @@
+# Liste initiale des notes
+notes = [15, 18, 12, 20, 14, 16, 18, 12, 17, 19]
+
+# Partie 1 : Modification de la liste
+
+# TODO 1 : Ajoutez une nouvelle note (13) à la fin de la liste.
+
+
+# TODO 2 : Insérez une nouvelle note (10) au début de la liste.
+
+
+# TODO 3 : Supprimez la première occurrence de la note 12.
+
+
+# TODO 4 : Supprimez la note à l'indice 2.
+
+
+# TODO 5 : Répétez la liste trois fois pour créer une nouvelle liste.
+
+
+# Partie 2 : Accès et modifications
+
+# TODO 6 : Accédez et affichez la note à l'indice 3 de la liste.
+
+
+# TODO 7 : Remplacez la note à l'indice 5 par 14.
+
+
+# TODO 8 : Vérifiez si la note 20 est présente dans la liste.
+
+
+# TODO 9 : Comptez combien de fois la note 18 apparaît dans la liste.
+
+
+# TODO 10 : Affichez l'indice de la première occurrence de la note 19.
+
+
+# TODO 11 : Créez une copie de la liste des notes.
+
+
+# TODO 12 : Triez la liste des notes dans l'ordre croissant.
+
+
+# TODO 13 : Inversez l'ordre des notes dans la liste triée.
+
+
+# TODO 14 : Supprimez et affichez la dernière note de la liste.
+
+
+# TODO 15 : Effacez complètement la liste des notes.
+
+
+# Partie 3 : Fonctions appliquées aux listes
+
+# Recréons la liste des notes pour les prochaines questions
+notes = [15, 18, 12, 20, 14, 16, 18, 12, 17, 19]
+
+# TODO 16 : Calculez et affichez la somme des notes.
+
+
+# TODO 17 : Affichez la note la plus élevée et la plus basse dans la liste.
+
+
+# TODO 18 : Affichez le nombre total de notes dans la liste.
+
+
+# TODO 19 : Appliquez une fonction pour obtenir le carré de chaque note.
+
+
+# TODO 20 : Filtrez les notes pour ne garder que celles qui sont supérieures ou égales à 15.
+
+
+# TODO 21 : Vérifiez si toutes les notes sont supérieures ou égales à 10.
+
+
+# TODO 22 : Vérifiez s'il y a au moins une note supérieure à 18.
+
+from functools import reduce
+# TODO 23 : Calculez la somme cumulative des carés de toutes les notes pour obtenir un total.
+
