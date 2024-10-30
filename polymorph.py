f4ee10d55758ffeace6cb4c229dacc7152344a87
omar OUGHZAL <omar.oughzal@gmail.com>
2024-10-30 13:43:41 +0100

    commit

--- polymorph.py
+++ polymorph.py
@@ -1,8 +1,23 @@
-# Lire un fichier binaire (par exemple, une image)
-with open("image.png", "rb") as fichier_binaire:
-    contenu_binaire = fichier_binaire.read()
-    print(contenu_binaire)
+class EmptyClass:
+    pass # classe définie mais vide
 
-# Écrire dans un fichier binaire
-with open("nouvelle_image.png", "wb") as fichier_binaire:
-    fichier_binaire.write(contenu_binaire)
+class Stagiaire:
+    groupe = "DE101" # attribut de classe
+
+    def __init__(self, nom, prenom): # constructeur
+        self.nom = nom # attribut d'instance
+        self.prenom = prenom # attribut d'instance
+    
+    def __del__(self): # destructeur
+        print(f"objet détruit: {self.nom} {self.prenom}")
+
+    def affiche(self): # méthode d'instance
+        print(f"{self.nom} {self.prenom} est dans le groupe {self.groupe}")
+    @classmethod
+    def change_groupe(cls, groupe): # méthode de classe
+        cls.groupe = groupe
+    
+Stagiaire1 = Stagiaire("Doe", "John") # instanciation
+Stagiaire2 = Stagiaire("Doe", "Jane") # instanciation
+Stagiaire3 = Stagiaire1 # nouvelle référence à Stagiaire1
+
